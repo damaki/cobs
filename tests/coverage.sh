3fc1001c3f5e20278ad578388887d43dbe41d92b
Daniel King <damaki.gh@gmail.com>
2026-02-22 16:32:13 +0000

    Fix issues running coverage in CI

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+set -e
+
+alr build --profiles=*=validation
+
 alr gnatcov instrument --level=stmt+mcdc --dump-trigger=atexit --projects cobs.gpr
 
-alr build -- --src-subdirs=gnatcov-instr --implicit-with=gnatcov_rts_full
+alr build --profiles=*=validation -- --src-subdirs=gnatcov-instr --implicit-with=gnatcov_rts_full
 
 alr exec -- bin/unit_tests
 
